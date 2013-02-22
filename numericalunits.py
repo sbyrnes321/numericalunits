295e5562e9f26fa104ab73eebde274dd3efe6fba
Steve <steve@steve-Inspiron14z.(none)>
2013-02-22 08:15:18 -0500

    fix packaging mistake, bump version number to 1.11

--- numericalunits.py
+++ numericalunits.py
@@ -14,7 +14,7 @@ For information and usage see README, or http://pypi.python.org/pypi/numericalun
 from math import pi
 import random
 
-__version__ = 1.1
+__version__ = 1.11
 
 def reset_units(seed=None):
     """
